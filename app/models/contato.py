5122093198910a36cff4d66454f780d0496c1335
igsilvs <igorss2008@gmail.com>
2020-10-15 20:16:05 -0300

    Criando Regra de Negocio

--- app/models/contato.py
+++ app/models/contato.py
@@ -1,4 +1,9 @@
 class Contato(object):
     def __init__(self, nome_contato, numero_telefone):
+        if len(nome_contato) > 200:
+            raise ValueError("Nome deve ser menor que 200 caracteres")
+        if len(numero_telefone) > 25:
+            raise ValueError("Telefone deve ser menor que 25 caracteres")
+        
         self.nome_contato = nome_contato
         self.numero_telefone = numero_telefone
